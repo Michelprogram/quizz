aa38b2cba2f1cdf01e3d998650c765dc4ffcbc07
Daryl Caruso <daryltron@gmail.com>
2021-11-27 15:40:30 +0000

    Replace script2.js

--- Quizz/script2.js
+++ Quizz/script2.js
@@ -1,12 +1,19 @@
 const divContainer = document.querySelector('.app')
 
 window.addEventListener('load', ()=>{
+    const modele = new Modele();
+    const domManager = new DomManager(divContainer,modele)
+    const quizz = new Quizz(modele.questions,modele)
 
-    const domManager = new DomManager(divContainer)
-
-    const quizz = new Quizz(domManager,questions)
-
-
-    domManager.button.addEventListener('click', _ => quizz.checkAnswer(domManager.input.value))
-
+    //bouton
+    domManager.button.addEventListener('click', () => {
+        quizz.checkAnswer(domManager.input.value);
+        domManager.updateUI();});
+    
+    //minuteur
+    window.addEventListener('updateMinuteur',(e) => domManager.updateMinuteur(e.detail));//met à jour le minuteur chaque seconde
+    window.addEventListener('tempsEcoule',()=>{
+        quizz.nextQuestion();
+        domManager.updateUI();
+    });//passe à la question suivante quand le temps est écoulé
 })
