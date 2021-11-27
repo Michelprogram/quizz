8b1b75a8df7cf06ad16699da5bb8a0d62732d462
Daryl Caruso <daryltron@gmail.com>
2021-11-27 18:39:43 +0000

    Upload New File

---
+++ Quizz/modele.js
@@ -0,0 +1,14 @@
+class Modele{
+    questions = [
+        { question:"Combien font 9 + 10 ?", response: "19" },
+        { question:"C'est quoi un thread ?" , response: "un processus"},
+        { question:"Qui a créé git ?", response: "linus torvald"},
+        { question:"Il a cracké énigma ?", response: "alan turing"},
+        { question:"", response: ""}]
+    response = " "
+    label = " "
+
+    constructor(){
+        this.label = this.questions[0].question;
+    }
+}
